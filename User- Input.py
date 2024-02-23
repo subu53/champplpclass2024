803fc438e5b9cc6fddc95dc21dd0feac62c9e954
subu53 <subusam5@gmail.com>
2024-02-23 12:28:38 +0300

    Create User- Input.py
    Python Introduction
    1st week Coding exercise.
    
    Asking and printing user's name, age and Location.

---
+++ User- Input.py
@@ -0,0 +1,8 @@
+#1st week Coding exercise.
+#Asking and printing user's name, age and Location.
+
+name = input("Whats your Name:- ")
+age = input("Whats your Age:- ")
+location = input("Where do you live:- ")
+
+print("Hello",name, "you are", age, " years old and live in", location, ".")
