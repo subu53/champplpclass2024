f30b46fe889b149a88496e79fc2707a5fedec472
subu53 <subusam5@gmail.com>
2024-04-01 15:48:46 +0300

    Add files via upload

---
+++ Assignment_wk_4.py
@@ -0,0 +1,7 @@
+
+class Person:
+    def introduce(self, name, age, gender):
+        print("Name:-", name, "Age:-", age, "Gender:-", gender)
+
+Person_class = Person()
+Person_class.introduce("Sammy",99, "Male")
